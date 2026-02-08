0064cc0794b2374da1a76615881d55c27bc1ce33
José Álvaro Diaz Ponce <aldipo7292@gmail.com>
2026-02-08 23:17:15 +0000

    esta bien ahora el orden, quita la foto dentro de las tarjetas y pon el

--- src/components/wedding/event-card.tsx
+++ src/components/wedding/event-card.tsx
@@ -41,7 +41,7 @@ const EventCard: React.FC<EventCardProps> = ({
 
   return (
     <Card 
-      className="text-card-foreground rounded-none shadow-none border-none flex flex-col bg-card/90 backdrop-blur-lg"
+      className="text-card-foreground rounded-none shadow-none border-none flex flex-col bg-card"
     >
       <CardContent className="p-8 sm:p-12 flex-grow flex flex-col text-center items-center relative">
         <div className="mb-6 h-24 w-24 flex items-center justify-center">
