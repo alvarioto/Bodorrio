3fec8a00b17ce0cff2fe818736814ebff8dbd9ec
José Álvaro Diaz Ponce <aldipo7292@gmail.com>
2026-02-08 21:47:39 +0000

    puedes sustituir la linea esa <div class="w-full flex justify-center py-

--- src/components/wedding/divider.tsx
+++ src/components/wedding/divider.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Divider = () => {
   return (
-    <div className="w-full flex justify-center py-8">
-      <svg width="200" height="20" viewBox="0 0 200 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 10 C 20 20, 30 0, 50 10 S 70 20, 100 10 S 130 0, 150 10 S 180 20, 200 10" stroke="hsl(130, 20%, 65%)" strokeWidth="1.5" />
-      </svg>
+    <div className="w-full flex justify-center py-4">
+      <Image
+        src="/floral-banner.png"
+        alt="Separador floral"
+        width={300}
+        height={63}
+        className="w-auto h-auto opacity-70"
+      />
     </div>
   );
 };
